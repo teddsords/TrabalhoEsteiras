edae11a28f9329e645015a5ceabbaed1f2cd4bd0
sovm <teddyonu@hotmail.com>
2020-10-28 17:41:22 -0300

    Multi and Single thread version to turn in.

--- main.c
+++ main.c
@@ -82,17 +82,17 @@ int main()
 
     finishGeral = clock();
     timeGeral = ((double) (finishGeral - startGeral)) / CLOCKS_PER_SEC;
-    //printf("Deseja executar novamente? (0 para parar e 1 para continuar).\n");//Perguntando se o programa ira executar novamente
-    //scanf("%d", &opcaoContinuar);                               // Lendo input do teclado
-    //while((c = getchar()) != '\n' && c != EOF){}                // Limpando buffer
-    //if(opcaoContinuar == 1)                                     // Caso a condicao seja verdadeira,
-    //{
-    // quantidadeDeProdutosPassados = 0;                          // Iremos reiniciar a contagem
-    // for(indice = 0; indice < QTDPRODUTOS; indice++)
-    // {
-    //   pesosDosProdutos[indice] = 0;                            // Reiniciar os pesos para evitar lixo de memoria
-    // }
-    //}
+    printf("Deseja executar novamente? (0 para parar e 1 para continuar).\n");//Perguntando se o programa ira executar novamente
+    scanf("%d", &opcaoContinuar);                               // Lendo input do teclado
+    while((caracterLixo = getchar()) != '\n' && caracterLixo != EOF){}                // Limpando buffer
+    if(opcaoContinuar == 1)                                     // Caso a condicao seja verdadeira,
+    {
+    quantidadeDeProdutosPassados = 0;                          // Iremos reiniciar a contagem
+    for(indice = 0; indice < QTDPRODUTOS; indice++)
+    {
+      pesosDosProdutos[indice] = 0;                            // Reiniciar os pesos para evitar lixo de memoria
+    }
+    }
   }while(opcaoContinuar == 1);
 
   printf("Tempos para programa Multithread.\n");
@@ -146,14 +146,14 @@ void* somandoProdutos(void * arg)                                 // Trecho de c
       pthread_mutex_lock(&lock);                                  // Bloquearemos a seguinte parte do codigo pois e a secao critica
       pesosDosProdutos[quantidadeDeProdutosPassados] = rand()%20 + 1;           // Peso aleatorio
       quantidadeDeProdutosPassados++;                             // Passou um produto, soma um na quantidade
-
+      pthread_mutex_unlock(&lock);
       if(quantidadeDeProdutosPassados == QTDPRODUTOS)             // Caso tenhamos atingido o objetivo
       {
         pthread_cond_signal(&contagemAtingida);                   // Temos que avisar a threadContadora para finalizar o processo de contagem
         printf("somandoProdutos(): thread: %ld, QTD: %d    Atingimos marca\n", id, quantidadeDeProdutosPassados);
       }
       //printf("somandoProdutos(): thread: %ld, QTD: %d       Desbloqueando mutex\n", id, quantidadeDeProdutosPassados); // So para ilustrar quem esta contando no momento
-      pthread_mutex_unlock(&lock);                                // Desbloqueando a secao critica
+      // pthread_mutex_unlock(&lock);                                // Desbloqueando a secao critica
       // Mandando fazer algum trabalho para alternar no mutex
       //usleep(1);
     }
--- mainSingleThread.c
+++ mainSingleThread.c
@@ -41,19 +41,19 @@ int main()
     timeSoma = ((double) (finishSoma - startSoma)) / CLOCKS_PER_SEC;
     printf("Valor total do peso dos produtos: %d\n",somaDosPesosDosProdutos);
 
-    // printf("Deseja executar novamente? (0 para parar e 1 para continuar)\n");
-    // scanf("%d\n",&opcaoContinuar);
-    // while((c = getchar()) != '\n' && c != EOF){}
-    //
-    // if(opcaoContinuar == 1)
-    // {
-    //   quantidadeDeProdutosPassados = 0;
-    //   for(i = 0; i < QTDPRODUTOS; i++)
-    //   {
-    //     pesosDosProdutos[i] = 0;
-    //   }
-    // }
-    //
+    printf("Deseja executar novamente? (0 para parar e 1 para continuar)\n");
+    scanf("%d\n",&opcaoContinuar);
+    while((c = getchar()) != '\n' && c != EOF){}
+
+    if(opcaoContinuar == 1)
+    {
+      quantidadeDeProdutosPassados = 0;
+      for(i = 0; i < QTDPRODUTOS; i++)
+      {
+        pesosDosProdutos[i] = 0;
+      }
+    }
+
   }while(opcaoContinuar == 1);
 
   finishGeral = clock();
